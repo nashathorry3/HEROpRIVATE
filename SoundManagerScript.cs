6cf22a29f691d9b3e0b63cf33f77e9eb00a8b6a7
nashathorry3 <54019361+nashathorry3@users.noreply.github.com>
2021-05-08 03:08:36 +0200

    Add files via upload

---
+++ SoundManagerScript.cs
@@ -0,0 +1,30 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame
+
+    public static AudioClip Fire;
+    static AudioSource audioSrc;
+    void Start()
+    {
+        Fire = Resources.load<AudioCLips>("fire");
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static void Playsound(string clip)
+    {
+        switch (clip)
+        {
+            case "fire":
+                audioSrc.playOneShot(fire);
+        }
+    }
+}*/
